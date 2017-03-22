675cf5d73c8e0260bf3111ed1e1f5c328eed6015
ping-li2 <ping.li2@hpe.com>
2017-03-22 18:08:21 +0800

    updated

--- vue-ele/src/App.vue
+++ vue-ele/src/App.vue
@@ -1,6 +1,9 @@
 <template>
   <div id="app">
     {{msg}}
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+    <el-button type="text">文字按钮</el-button>
   </div>
 </template>
 
@@ -16,30 +19,5 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
